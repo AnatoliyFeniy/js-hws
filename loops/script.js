05c334a421e703927f814c6b276d96dae6ddcf33
unknown <vthirdmail@gmail.com>
2026-01-19 11:41:19 +0200

    2 tasks

--- loops/script.js
+++ loops/script.js
@@ -1,1 +1,19 @@
-console.log("hw")
+//1
+let num1 = parseInt(prompt("Введіть перше число діапазону:"));
+let num2 = parseInt(prompt("Введіть друге число діапазону:"));
+let sum = 0;
+for (let i = num1; i <= num2; i++) {
+  sum += i;
+}
+alert(sum);
+
+//2
+let n1 = parseInt(prompt("Введіть перше число:"));
+let n2 = parseInt(prompt("Введіть друге число:"));
+let min = n1 < n2 ? n1 : n2;
+let nsd = 0;
+for(let i = 1; i <= min; i++){
+    if(n1 % i === 0 && n2 % i === 0){
+        nsd = i
+    }
+}
