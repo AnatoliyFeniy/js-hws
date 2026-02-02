c89220f0a3e5e2df88e9e6ef1745c304c7ce5516
unknown <vthirdmail@gmail.com>
2026-02-02 19:46:51 +0200

    5 tasks

--- functions/script.js
+++ functions/script.js
@@ -1,1 +1,45 @@
-console.log('as')
+//1
+const compare=function(a,b){
+    if(a < b){
+        return -1;
+    }
+    else if(a > b){
+        return 1;
+    }
+    else if(a === b){
+        return 0;
+    }
+}
+alert(compare(2,1));
+
+//2
+const fact = n => n === 1 ? 1 : n * fact(n - 1);
+console.log(fact(5));
+
+//3
+const unsplit = (n1,n2,n3) => console.log(`${n1}${n2}${n3}`)
+unsplit(4,2,3)
+
+//4
+const trsq=function(a, b){
+    if(a != undefined && b != undefined){
+        return `Площа трикутника: ${(a * b) / 2}`;
+    }
+    else if(b === undefined){
+        return `Площа квадрату: ${(a * a) / 2}`;
+    }
+    else if(a === undefined){
+        return `Площа квадрату: ${(b * b) / 2}`;
+    }
+}
+console.log(trsq(5,4));
+
+//5
+const perfect=function(x){
+    for(let i = 1; i < x; i++){
+        if(x % i === 0){
+            console.log(i);
+        }
+    }
+}
+perfect(28);
