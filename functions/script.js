a3294e08c950c575117daed5ddb09f8d232a2896
unknown <vthirdmail@gmail.com>
2026-02-02 23:37:29 +0200

    6 tasks

--- functions/script.js
+++ functions/script.js
@@ -43,3 +43,19 @@ const perfect=function(x){
     }
 }
 perfect(28);
+
+//6
+const minmax=function(a,b){
+    for(let i = a; i <= b; i++){
+        let sum = 0
+        for(let j = 1; j < i; j++){
+            if(i % j === 0){
+                sum += j;
+            }
+        }
+        if(sum === i){
+            console.log(i, "є досконалим числом");
+        }
+    }
+}
+minmax(10,10000);
