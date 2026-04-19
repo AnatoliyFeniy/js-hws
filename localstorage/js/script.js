05d5bd0d9554765b9ba4170d196944e72d4cef2a
unknown <vthirdmail@gmail.com>
2026-04-19 19:56:25 +0300

    3 tasks

--- localstorage/js/script.js
+++ localstorage/js/script.js
@@ -21,3 +21,29 @@ items.forEach((item) => {
 lp.textContent = localStorage.getItem("itm") 
     ? `Останній переглянутий: ${localStorage.getItem("itm")}` 
     : "";
+const tp1 = document.getElementById("tp1");
+const tp2 = document.getElementById("tp2");
+const tbtn = document.getElementById("tbtn")
+const startTimer = function(){
+    const interval = setInterval(()=>{
+    const startTime = localStorage.getItem("startTime");
+    const elapsed = Math.floor((Date.now() - startTime) / 1000);
+    const timeLeft = 60 - elapsed;
+    if(timeLeft <= 0){
+        tp1.textContent = "60";
+        tp2.textContent = "Час вийшов";
+        clearInterval(interval);
+        localStorage.removeItem("startTime");
+    }
+    else{
+        tp1.textContent = `Часу залишилося: ${timeLeft}с`
+    }
+}, 1000);
+if (localStorage.getItem("startTime")) {
+    startTimer();
+}
+}
+tbtn.addEventListener('click', ()=>{
+    localStorage.setItem("startTime", Date.now());
+    startTimer();
+});
