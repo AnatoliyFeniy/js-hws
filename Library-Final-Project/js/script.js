6dda34f10b6c8c949e721f38378913193ca0655f
unknown <vthirdmail@gmail.com>
2026-04-23 20:45:21 +0300

    second commit

--- Library-Final-Project/js/script.js
+++ Library-Final-Project/js/script.js
@@ -25,29 +25,35 @@ addBtn.addEventListener("click", function(){
         top: 50%;
         left: 50%;
         transform: translate(-50%, -50%);
-        width: 900px;
-        height: 1000px;
-        background: rgba(150, 75, 0, 1);
+        width: 800px;
+        height: 950px;
+        background: radial-gradient(circle, black -30%, rgba(150, 75, 0, 1) 100%);
         border-radius: 20px;
+        border-style: groove;
+        border: 2px solid black;
         padding: 25px;
         overflow-y: auto;
         z-index: 1000;
     `;
     modal.innerHTML = `
-        <h2>Додати книгу</h2>
-        <input type="text" id="bookTitle" placeholder="Назва книги">
-        <input type="number" id="bookYear" placeholder="Рік">
-        <input type="text" id="bookAuthor" placeholder="Автор">
-        <input type="text" id="bookPublisher" placeholder="Видавництво">
-        <input type="text" id="bookImg" placeholder="Посилання на картинку">
-
-        <h2>Дані користувача</h2>
-        <input type="text" id="userFirstName" placeholder="Ім'я">
-        <input type="text" id="userLastName" placeholder="Прізвище">
-        <input type="text" id="userPhone" placeholder="Номер телефону">
-
-        <button id="saveBtn">Зберегти</button>
-        <button id="closeBtn">Закрити</button>
+        <h2 class="formtitle">Додати книгу</h2>
+        <div class="bookform">
+        <input class="forminpt" type="text" id="bookTitle" placeholder="Назва книги">
+        <input class="forminpt" type="number" id="bookYear" placeholder="Рік">
+        <input class="forminpt" type="text" id="bookAuthor" placeholder="Автор">
+        <input class="forminpt" type="text" id="bookPublisher" placeholder="Видавництво">
+        <input class="forminpt" type="text" id="bookImg" placeholder="Посилання на картинку">
+        </div>
+        <h2 class="formtitle2">Дані користувача</h2>
+        <div class="userform">
+        <input class="forminpt" type="text" id="userFirstName" placeholder="Ім'я">
+        <input class="forminpt" type="text" id="userLastName" placeholder="Прізвище">
+        <input class="forminpt" type="text" id="userPhone" placeholder="Номер телефону">
+        </div>
+        <div class="fbtns">
+        <button class="fbtn" id="saveBtn">Зберегти</button>
+        <button class="fbtn2" id="closeBtn">Закрити</button>
+        </div>
     `;
     document.body.appendChild(modal);
 
